6f2e171a14813a02523ed48d8f30df316d2e5033
anonymous <xgqfrms@gmail.com>
2017-09-26 10:12:00 +0800

    Update and rename Tutorials/GraphQL.js to Tutorials/Demos/GraphQL.js

--- Tutorials/Demos/GraphQL.js
+++ Tutorials/Demos/GraphQL.js
@@ -3,9 +3,9 @@
 var {graphql, buildSchema} = require('graphql');
 
 var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Query {
+        hello: String
+    }
 `);
 
 var root = {
